423f409587ff8e3586384b8d808d1228e4b1eac3
FabioStorer <fabiostorer19@gmail.com>
2025-08-28 18:56:55 -0300

    errorhandler

--- src/controllers/example_controllers.ts
+++ src/controllers/example_controllers.ts
@@ -4,4 +4,8 @@ export class ExampleController {
     async helloWorld(req: Request, res: Response) {
         return res.send('Hello Typescript.')
     };
+
+    testError( req: Request, res: Response) {
+        throw new Error('Teste de erro');
+    }
 };
--- src/routes/example_routes.ts
+++ src/routes/example_routes.ts
@@ -6,3 +6,4 @@ export const exampleRoutes: Router = Router();
 const exampleController = new ExampleController();
 
 exampleRoutes.get('/hello-world', exampleController.helloWorld);
+exampleRoutes.get('/error-test', exampleController.testError);
